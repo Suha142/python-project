c68a3aa0af8c4637c8e97d1c302eff32cc5d96fd
Jannatul Ferdoush <52478206+Suha142@users.noreply.github.com>
2019-07-17 02:32:23 +0600

    completed (not exactly)

--- SchoolInfoSystem/schools/views.py
+++ SchoolInfoSystem/schools/views.py
@@ -12,7 +12,29 @@ def login(request):
     return render(request, 'schools/login.html')
 def main(request):
     s = Schools.objects.all()
-    context = {'all_schools': s}
+    s1 = Schools.objects.filter(location__contains='armg')
+    s2 = Schools.objects.filter(location__contains='hammadpur')
+    s3 = Schools.objects.filter(location__contains='anmondi')
+    s4 = Schools.objects.filter(location__contains='ulshan')
+    s5 = Schools.objects.filter(location__contains='nani')
+    s6 = Schools.objects.filter(location__contains='tijheel')
+    s7 = Schools.objects.filter(location__contains='ari')
+    s8 = Schools.objects.filter(location__contains='irpur')
+    s9 = Schools.objects.filter(location__contains='ttara')
+    s10 = Schools.objects.filter(location__contains='bo')
+    a = Admin.objects.all()
+    context = {'all_schools': s,
+               'in_farmgate': s1,
+               'in_mdpur': s2,
+               'in_dhanmondi': s3,
+               'in_gulshan': s4,
+               'in_bonani': s5,
+               'in_motjhl': s6,
+               'in_wari': s7,
+               'in_mirpur': s8,
+               'in_uttara': s9,
+               'in_bashaboo': s10,
+               'user': a}
     return render(request, 'schools/main.html' , context)
 def addSchools(request):
     if "add" in request.method == 'post':
